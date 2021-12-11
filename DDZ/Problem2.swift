b08606affcc032d1e51c54388d1f5c3379716f9e
Zed Null <rustamxaxyk@gmail.com>
2021-12-11 19:46:08 +0300

    problem2.2

--- DDZ/Problem2.swift
+++ DDZ/Problem2.swift
@@ -1,28 +1,56 @@
 import Foundation
 
-private var solution2: String = ""
+private var solution2_1: String = ""
+private var solution2_2: String = ""
 
-private func problem2(i: Int, k: Int, prefix: String) {
+private func problem2_1(i: Int, k: Int, prefix: String) {
     if i == 5 {
         if k == 1 {
-            solution2 += prefix + "a" + "\n"
+            solution2_1 += prefix + "a" + "\n"
         } else {
             ["b","c","d","e","f"].forEach { ch in
-                solution2 += prefix + ch + "\n"
+                solution2_1 += prefix + ch + "\n"
             }
         }
     } else if i == 4 && k == 0 {
-        problem2(i: i + 1, k: 1, prefix: prefix + "a")
+        problem2_1(i: i + 1, k: 1, prefix: prefix + "a")
     } else {
         ["a","b","c","d","e","f"].forEach { ch in
             if !(k == 2 && ch == "a") {
-                problem2(i: i + 1, k: k + (ch == "a" ? 1 : 0), prefix: prefix + ch)
+                problem2_1(i: i + 1, k: k + (ch == "a" ? 1 : 0), prefix: prefix + ch)
+            }
+        }
+    }
+}
+
+private func problem2_2(i: Int, k: Int, prefix: String, used: String) {
+    if i == 5 {
+        if k == 1 {
+            solution2_2 += prefix + "a" + "\n"
+        } else {
+            ["b","c","d","e","f"].forEach { ch in
+                if !used.contains(ch) {
+                    solution2_2 += prefix + ch + "\n"
+                }
+            }
+        }
+    } else if i == 4 && k == 0 {
+        problem2_2(i: i + 1, k: 1, prefix: prefix + "a", used: used)
+    } else {
+        ["a","b","c","d","e","f"].forEach { ch in
+            if !used.contains(ch) {
+                if !(k == 2 && ch == "a") {
+                    problem2_2(i: i + 1, k: k + (ch == "a" ? 1 : 0), prefix: prefix + ch, used: ch == "a" ? used : used + ch)
+                }
             }
         }
     }
 }
 
 func Problem2() {
-    problem2(i: 1, k: 0, prefix: "")
-    try! solution2.write(to: Bundle.main.executableURL!.deletingLastPathComponent().appendingPathComponent("solution2.txt"), atomically: true, encoding: .utf8)
+    problem2_1(i: 1, k: 0, prefix: "")
+    try! solution2_1.write(to: Bundle.main.executableURL!.deletingLastPathComponent().appendingPathComponent("solution2_1.txt"), atomically: true, encoding: .utf8)
+
+    problem2_2(i: 1, k: 0, prefix: "", used: "")    
+    try! solution2_2.write(to: Bundle.main.executableURL!.deletingLastPathComponent().appendingPathComponent("solution2_2.txt"), atomically: true, encoding: .utf8)
 }
